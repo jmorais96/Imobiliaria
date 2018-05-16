14286fe04c7f1977e2d2e7cbe00f8630d5be7d28
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-05-16 15:24:33 +0000

    Início da implementação de Bootstrap no projeto (precisamente no header da homepage)

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 
     <!-- MetaTags -->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
     <!-- Folhas de estilo -->
     <link rel="stylesheet" href="css/homepage.css" type="text/css">
@@ -27,30 +31,47 @@
 
   <body>
 
-  <!-- HEADER -->
-  <div class="container_header">
-     
-     <div class="header">
-            <div class="icon">
-              <a href="index.php"><img id="icon" src="images/logo.png"/></a>
-              <p id="homeIconName">
-            </div>
+  <!-- HEADER/NAVBAR --> 
+  <div class="container-header">
+  <nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="index.php"><img id="icon" src="images/logo.png"/></a>
 
-            <div class="navItems">
-                <a href="index.php"><p id="navItem">Home</p></a>
-                 <a href="register.php"><p id="navItem">Registo</p></a>
-                <a href="logout.php"><p id="navItem">Login</p></a>
-            </div>
+  <!-- Toogler que aparecerá nos menores ecrãs -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span></button>
 
-            <div class="phone">
-              <img id="phoneIcon" src="images/call-answer.svg"/>
-              <p id="phone-number">296 012 345</p>
-            </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
+        <ul class="navbar-nav mx-auto">
+
+        <!-- Link de navegação "Home" -->
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        
+        <!-- Link de navegação "Registo" -->
+        <li class="nav-item">
+            <a class="nav-link" href="register.php">Registo</a>
+        </li>
+        
+        <!-- Link de navegação que abre o módulo de "Login" -->    
+        <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+        </li>
+
+        </ul>
+
+        <div class="phone">
+            <img id="phoneIcon" src="images/call-answer.svg" alt="Contacto Telefónico"/>
+            <p id="phone-number">296 012 345</p>
         </div>
-  
-  </div>
-  <!-- FINAL DO HEADER -->
+
+    </div>
+
+    </nav>
+</div>
+    <!-- FINAL DO HEADER/NAVBAR  -->
+
 
    <!-- ÁREA CENTRAL DA PÁGINA -->
    <div class="map" style="float:left;"></div>
@@ -149,6 +170,17 @@
 
   <!-- API Google Maps -->
   <script src="http://maps.google.com/maps/api/js?key=AIzaSyDrXJ1v5Tyan8210Bl76AnTl0HdcK0BdEY&callback=initMap"></script>
+
+    <!-- Ficheiros JavaScript pessois -->
+    
+    <!-- jQuery -->  
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <!-- Popper.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
     <?php
 
