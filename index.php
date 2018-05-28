f97484f0971257b183cdca7ecfd940792482d628
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-05-28 09:20:30 +0000

    Layout da homepage corrigido (header e mapa normalizados e não sobrepostos)

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
             
             <!-- Header do módulo --> 
             <div class="modal-header">
-                <h3 class="modal-title">Efetue o seu login</h3>
+                <h3 class="modal-title">Efetue o seu log in</h3>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
