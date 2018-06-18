78a0b97a198a79e195dcbec13af0f908f322c8f5
Jose Morais <jose.morais.96@hotmail.com>
2018-06-18 06:25:26 +0000

    bug fix manager separador

--- administrator/management/manager.php
+++ administrator/management/manager.php
@@ -34,41 +34,45 @@ session_start();
     </div>
     <div class="admin_container">
     <div class="tab">
-      <button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">Imóveis</button>
-      <button class="tablinks" onclick="openCity(event, 'Paris')">Adicionar imóveis</button>
-      <button class="tablinks" onclick="openCity(event, 'Tokio')">Visitas</button>
+      <button class="tablinks"  id="btnImoveis">Imóveis</button>
+      <button class="tablinks" id="btnAdicionarImovel" >Adicionar imóveis</button>
+      <button class="tablinks" id="btnAdicionarVisita">Visitas</button>
     </div>
-    <div id="London" class="tabcontent">
+    <div id="imoveis" class="tabcontent">
         <div class="management">
 
           <div class="thumbnail_management">
             <div class="thumb_img_management">
-              <a href="../../p_imovel.php?id=<?php echo $data[0]; ?>"><img src="../../imoveis/<?php echo $data[0] ?>/<?php echo $data[0] ?>_0.jpg" class="img_pesquisa_m" alt=""></a>
+              <a href="../../p_imovel.php?id="><img src="../../imoveis/_0.jpg" class="img_pesquisa_m" alt=""></a>
                         </div>
             <div class="thumbnail_info_management">
-              <p><?php echo $data[1] ?> - <?php echo $data[2] ?></p>
-              <p><?php echo $data_ilha[0] ?> - <?php echo $data_concelho[0] ?> - <?php echo $data_freguesia[0] ?></p>
-              <p><?php echo $data[6] ?></p>
-              <p><?php echo substr($data[9],0,25)."..."; ?></p>
-              <p><?php echo $data[7] ?></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
+              <p></p>
             </div>
             <div class="buttons_management">
-              <a href="propor.php?id=<?php echo $data[0]; ?>"> <button class="ask_for_feature"type="button" name="button">Propor a destaque</button></a>
-              <a href="../../edicao_imovel.php?id=<?php echo $data[0]; ?>"><button class="edit" type="button" name="button">Editar</button></a>
-              <a href="../../eliminar_imovel.php?id=<?php echo $data[0]; ?>"><button class="delete" type="button" name="button">Eliminar</button></a>
+              <a href="propor.php?id="> <button class="ask_for_feature"type="button" name="button">Propor a destaque</button></a>
+              <a href="../../edicao_imovel.php?id="><button class="edit" type="button" name="button">Editar</button></a>
+              <a href="../../eliminar_imovel.php?id="><button class="delete" type="button" name="button">Eliminar</button></a>
 
-              <button class="visits_noti">visitas(<?php echo count($visitas); ?>)</button>
+              <button class="visits_noti">visitas()</button>
               <div class="notifications_box1">
                 <div class="notification1">
-                  <p><?php echo $data_cli[1]." ".$data_cli[2] ; ?>, quer visitar este imóvel dia <?php echo "$value1[4]"; ?> às <?php echo "$value1[5]";  ?></p>
+                  <p>, quer visitar este imóvel dia às </p>
                   <div id="visit_aprovation">
-                    <button type="button" class="aprove_visit"> <a href="aceitar_visita.php?id=<?php echo $value1[0]; ?>">v</a></button>
-                    <button type="button" class="disaprove_visit"><a href="negar_visita.php?id=<?php echo $value1[0]; ?>">x</a></button>
+                    <button type="button" class="aprove_visit"> <a href="aceitar_visita.php?id=">v</a></button>
+                    <button type="button" class="disaprove_visit"><a href="negar_visita.php?id=">x</a></button>
                   </div>
                 </div>
           </div>
       </div>
-    <div id="Paris" class="tabcontent">
+    </div>
+    </div>
+  </div>
+
+    <div id="adicionarImovel" class="tabcontent">
         <form class="add_property" action="" method="post" enctype="multipart/form-data" >
           <div class="add_prop_box">
           <div><label>Finalidade</label></div>
@@ -138,20 +142,43 @@ session_start();
         </div>
         </form>
     </div>
-    <div id="Tokio" class="tabcontent">
+    <div id="visitas" class="tabcontent">
 
     </div>
-    </div>
 
-    <script src="../../js/admin_container.js"></script>
-    <script src="../../js/filter.js"></script>
-    <script src="../../js/popup.js"></script><!-- Script para login -->
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script>
-    $(document).on("click", ".visits_noti", function(event){
-        event.preventDefault();
-        $(this).next('.notifications_box1').toggle();
-    });
+      $(document).on("click", ".visits_noti", function(event){
+          event.preventDefault();
+          $(this).next('.notifications_box1').toggle();
+      });
+
+      $("#imoveis").show();
+      $("#visita").hide();
+      $("#adicionarImovel").hide();
+
+
+      $("#btnAdicionarImovel").click(function(){
+        $("#imoveis").hide();
+        $("#visita").hide();
+        $("#adicionarImovel").show();
+      });
+
+
+      $("#btnImoveis").click(function(){
+        $("#adicionarImovel").hide();
+        $("#visita").hide();
+        $("#imoveis").show();
+      });
+
+
+      $("#btnAdicionarVisita").click(function(){
+        $("#imoveis").hide();
+        $("#adicionarImovel").hide();
+        $("#visita").show();
+      })
+
 
     </script>
   </body>
