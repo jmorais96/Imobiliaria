33b73b268fb4ecf2a8791bde0a7a12c36188ed3e
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-06-18 21:48:59 +0000

    Header da página de gestão de conteúdos melhorado

--- assets/logout.php
+++ assets/logout.php
@@ -3,7 +3,7 @@
   if (isset($_GET['acao'])) {
     if ($_GET['acao']=='logout') {
       session_destroy();
-      header('location:index.php');
+      header('location:../../index.php');
     }
   }
 
