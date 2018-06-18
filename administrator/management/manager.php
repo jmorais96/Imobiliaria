9107ecbc4b8ff0fce17c4af20bbdbcfab640c664
Jose Morais <jose.morais.96@hotmail.com>
2018-06-18 07:22:26 +0000

    mapa form adicionar imovel

--- administrator/management/manager.php
+++ administrator/management/manager.php
@@ -7,16 +7,29 @@ session_start();
   if (!isset($_SESSION['funcionario'])) {
     header("location:../index.php");
   }
+
+  $bd=new imobiliaria("../../data/config.ini");
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">Azores Property | Gestão de conteúdos</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" media='screen and (min-width: 260px) and (max-width: 767px)' href="../../css/mobile.css"/>
-    <link rel="stylesheet" media='screen and (min-width: 768px) and (max-width: 1100px)' href="../../css/tablet.css"/>
-    <link rel="stylesheet" media='screen and (min-width: 1101px)' href="../../css/admin_desktop-property.css"/>
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <!-- Folhas de estilo -->
+    <link rel="stylesheet" href="../../css/homepage.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="../../css/gerirImovelTable.css">
+
+    <!-- Ícones Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <!-- Ficheiros JavaScript -->
+    <script src="../../js/jquery.js"></script>
+    <script src="../../js/main.js"></script>
   </head>
   <body>
     <div class="nav_box">
@@ -73,53 +86,41 @@ session_start();
   </div>
 
     <div id="adicionarImovel" class="tabcontent">
-        <form class="add_property" action="" method="post" enctype="multipart/form-data" >
+        <form class="add_property" action="" method="get" enctype="multipart/form-data" >
           <div class="add_prop_box">
           <div><label>Finalidade</label></div>
               <select  name="finalidade">
-                <option value="">Finalidade</option>
-                <option value="arrendar">arrendar</option>
-                <option value="comprar">comprar</option>
+                <?php $bd->selectFinalidade() ?>
               </select>
             </div>
           <div class="add_prop_box">
           <div><label>Tipo de imóvel</label></div>
-            <select  name="tipo_imovel" id="tipo_de_imovel" onchange="functionTipologia(this.id, 'tipologia')">
-              <option value="">Tipo de imóvel</option>
-              <option value="Terreno">Terrenos</option>
-              <option value="Apartamento">Apartamentos</option>
-              <option value="Moradia">Moradias</option>
-              <option value="Quinta">Quintas</option>
+            <select  name="tipo_imovel" id="tipo_de_imovel">
+              <?php $bd->selectTipoImovel() ?>
             </select>
           </div>
         <div class="add_prop_box">
           <div><label>Tipologia</label></div>
             <select  name="tipologia" id="tipologia">
-              <option value="">Tipologia</option>
-              <option value="T0">T0</option>
-              <option value="T1">T1</option>
-              <option value="T2">T2</option>
-              <option value="T3">T3</option>
-              <option value="T4">T4</option>
-              <option value="T5">T5</option>
+              <?php $bd->selectTipologia() ?>
             </select>
           </div>
         <div class="add_prop_box">
           <div><label>Ilha</label></div>
-            <select  name="ilha" id="ilha" onchange="functionConcelho(this.id,'concelho')">
-              <option value="">Ilha</option>
+            <select  name="ilha" id="ilha" >
+              <?php $bd->selectIlha(); ?>
             </select>
           </div>
         <div class="add_prop_box">
           <div><label>Concelho</label></div>
-            <select  name="concelho" id="concelho" onchange="functionFreguesia(this.id,'freguesia')">
-              <option value="Concelho">concelho</option>
+            <select  name="concelho" id="concelho" >
+              <option value="">Selecione um concelho</option>
             </select>
           </div>
         <div class="add_prop_box">
           <div><label>Freguesia</label></div>
             <select  name="freguesia" id="freguesia">
-              <option value="Freguesia">freguesia</option>
+              <option value="">Selecione uma freguesia</option>
             </select>
           </div>
         <div class="add_prop_box">
@@ -137,6 +138,12 @@ session_start();
       <div class="add_prop_box">
           <div><label>Imagem(s)</label></div><br><input type="file" name="img[]" accept="image/*" multiple>
         </div>
+        <div class="add_prop_box">
+            <input type="hidden" name="lat" value="">
+            <input type="hidden" name="lng" value="">
+          </div>
+        <!-- MAPA -->
+      <div class="map" style="height:500px;"></div>
       <div class="add_prop_box">
           <input type="submit" value="criar"/>
         </div>
@@ -147,11 +154,53 @@ session_start();
     </div>
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <!-- API Google Maps -->
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyDrXJ1v5Tyan8210Bl76AnTl0HdcK0BdEY&callback=initMap"></script>
+
+      <!-- Ficheiros JavaScript pessois -->
+
+      <!-- jQuery -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+      <!-- Latest compiled Jquery -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+      <!-- Popper.js -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+      <!-- Bootstrap JS -->
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+
     <script>
       $(document).on("click", ".visits_noti", function(event){
           event.preventDefault();
           $(this).next('.notifications_box1').toggle();
+      });
+
+      $("#ilha").change(function(){
+          let ilha = $("#ilha").val();
+          $.ajax({
+          type:'POST',
+          url:'../../assets/concelho.php',
+          data:"idIlha="+ ilha,
+          success:function(html){
+              $('#concelho').html(html);
+          }
+          });
+      });
+
+      $("#concelho").change(function(){
+          let concelho = $("#concelho").val();
+          $.ajax({
+          type:'POST',
+          url:'../../assets/freguesia.php',
+          data:"idConcelho="+ concelho,
+          success:function(html){
+              $('#freguesia').html(html);
+
+          }
+          });
       });
 
       $("#imoveis").show();
@@ -177,8 +226,31 @@ session_start();
         $("#imoveis").hide();
         $("#adicionarImovel").hide();
         $("#visita").show();
-      })
+      });
 
+      marker="";
+      google.maps.event.addListener(map, 'click', function(event) {
+        if (marker == "") {
+          placeMarker(event.latLng);
+        }else {
+          marker.setMap(null);
+          marker="";
+          placeMarker(event.latLng);
+        }
+      });
+
+      function placeMarker(location) {
+          marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        //console.log(location.toString());
+        let coordenadas=location.toString().replace("(", "").replace(")", "").split(' ').join('').split(",");
+        //console.log(coordenadas);
+        console.log(coordenadas[0]);
+        $("[name=lat]").val(coordenadas[0]);
+        $("[name=lng]").val(coordenadas[1]);
+      }
 
     </script>
   </body>
