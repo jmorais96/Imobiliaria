a5c4bbb23eff54992cc7074d423b3bce6c1030d8
Carina Gomes <cfagomes@gmail.com>
2018-06-19 19:50:21 +0000

    update gestor

--- data/imobiliaria.class.php
+++ data/imobiliaria.class.php
@@ -526,6 +526,20 @@
         return true;
 
     }
+      
+    public function editarGestor($mail, $pass, $nome, $sobrenome, $contact){
+
+        $sql = 'SELECT idTipoUser FROM tipo_user WHERE tipo = :tipo';
+        $tipo = $this->query($sql, array(":tipo" => "Gestor"));
+
+        $sql ='UPDATE utilizador SET email = :email, nomeProprio = :nomeProprio, sobrenome = :sobrenome, password = :password, contacto = :contacto';
+
+        $arr = array('email' => ($mail) , 'password' => md5($pass), 'nomeProprio' => ($nome), 'sobrenome' => ($sobrenome), 'contacto' => ($contact), 'tipoUser' => $tipo[0]['idTipoUser']);
+        $this->query($sql, $arr);
+
+       // return true;
+
+    }
 
     public function mailGestorExists($mail){
       $sql='select count(*) from funcionario where email = :email';
@@ -552,7 +566,7 @@
 
     }
 
-
+ 
        public function getWorkers(){
         $sql = 'SELECT idTipoUser FROM tipo_user WHERE tipo = :tipo';
         $tipoUser = $this->query($sql, array(":tipo" => "Gestor"));
